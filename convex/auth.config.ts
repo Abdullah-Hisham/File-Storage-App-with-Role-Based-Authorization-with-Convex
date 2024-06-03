eadaf46e9d0e276b79e2c5e3d7798e0609e6bb4a
Abdullah_Hisham <abdulahhisham15@gmail.com>
2024-06-03 16:43:37 +0300

    initial setup of project

---
+++ convex/auth.config.ts
@@ -0,0 +1,23 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    providers: [
+      {
+        domain: "https://polite-calf-20.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
